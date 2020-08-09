c85c9314d6131bc3634cd6629665e480b47243bf
akorol98 <akorol98.17@gmail.com>
2020-08-10 00:06:49 +0200

    shower position added

--- GAN/models/dataUtils.py
+++ GAN/models/dataUtils.py
@@ -4,7 +4,7 @@ import pandas as pd
 from torch.utils.data import Dataset
 
 class PionsDataset(Dataset):
-    
+
     def __init__(self, path_list):
         self.path_list = path_list
         part = np.array([])
@@ -13,30 +13,33 @@ class PionsDataset(Dataset):
             file = h5py.File(path, 'r')['hcal_only/energy']
             part = np.append(part, np.ones(len(file))*i)
             index = np.append(index, np.arange(len(file)))
-            
-                
-#         self.keys = pd.DataFrame({'part' : part.astype(int),
-#                                   'index' : index.astype(int)
-#                                  })
-        
-        self.keys = np.vstack([part.astype(int), index.astype(int)]).T
+
+
+        self.keys = pd.DataFrame({'part' : part.astype(int),
+                                  'index' : index.astype(int)
+                                 })
 
     def __len__(self):
         return len(self.keys)
-    
+
     def __getitem__(self, idx):
-#         part = self.keys['part'][idx]
-#         index = self.keys['index'][idx]
-        part = self.keys[idx][0]
-        index = self.keys[idx][1]
+        part = self.keys['part'][idx]
+        index = self.keys['index'][idx]
         path = self.path_list[part]
-        
+
         file = h5py.File(path, 'r')['hcal_only']
         energy = file['energy'][index]
         shower = file['layers'][index]
-        
+
+        layer_energy = file['layers'][index].sum(axis=1).sum(axis=1)
+        try:
+            free_path = 48 - np.where(np.diff(layer_energy) > 11)[0][0] + 1
+        except:
+            free_path = 1
+
         energy = energy.reshape(1,1,1,1)
         shower = np.expand_dims(shower, 0)
 
         return {'energy' : energy,
-                'shower' : shower}
+                'shower' : shower,
+                'free_path' : free_path}
